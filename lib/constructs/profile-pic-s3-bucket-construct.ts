825ac6f8831651007bccededd6dea942d4fcefab
Alecio <aleciomadrid@Alecios-MacBook-Pro.local>
2024-10-26 17:19:16 -0600

    updates to profile pic function and create new user function

--- lib/constructs/profile-pic-s3-bucket-construct.ts
+++ lib/constructs/profile-pic-s3-bucket-construct.ts
@@ -1,22 +1,35 @@
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import { aws_s3 } from 'aws-cdk-lib';
 import { RemovalPolicy } from 'aws-cdk-lib';
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export interface ProfilePicS3BucketConstructProps {
-
+    updateProfilePicLambdaFunction: lambda.Function,
 }
 
 export class ProfilePicS3BucketConstruct extends Construct {
+    public readonly profilePicS3Bucket: s3.Bucket;
+
     constructor(scope: Construct, id: string, props: ProfilePicS3BucketConstructProps) {
         super(scope, id);
 
-        const pofilePicS3Bucket = new s3.Bucket(scope, 'ProfilePicBucket', {
+        this.profilePicS3Bucket = new s3.Bucket(scope, 'ProfilePicBucket', {
             bucketName: 'break-point-profile-pic-bucket',
             blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
             encryption: s3.BucketEncryption.S3_MANAGED,
             enforceSSL: true,
             versioned: true,
             removalPolicy: RemovalPolicy.RETAIN,
+            cors: [{
+                allowedHeaders: ["Authorization","*"],
+                allowedOrigins: ["*"],
+                allowedMethods: [aws_s3.HttpMethods.HEAD, aws_s3.HttpMethods.GET, aws_s3.HttpMethods.PUT],
+                exposedHeaders: [],
+                maxAge: 0
+            }]
         });
+
+        this.profilePicS3Bucket.grantPut(props.updateProfilePicLambdaFunction)
     }
 }
--- lib/stacks/profile-pic-stack.ts
+++ lib/stacks/profile-pic-stack.ts
@@ -3,13 +3,15 @@ import { Construct } from 'constructs';
 import { ProfilePicS3BucketConstruct } from '../constructs/profile-pic-s3-bucket-construct';
 import { UpdateProfilePicLambdaConstruct } from '../constructs/update-profile-pic-lambda-construct';
 
-
 export class ProfilePicStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const updateProfilePicLambdaConstruct = new UpdateProfilePicLambdaConstruct(this, 'UpdateProfilePicLambda', {});
 
-    const profilePicS3BucketConstruct = new ProfilePicS3BucketConstruct(this, 'ProfilePicS3Bucket', {});
+    const profilePicS3BucketConstruct = new ProfilePicS3BucketConstruct(this, 'ProfilePicS3Bucket', {
+        updateProfilePicLambdaFunction: updateProfilePicLambdaConstruct.updateProfilePicFunction,
+
+    })
   }
 }
